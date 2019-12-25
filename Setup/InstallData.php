cd16f4f17a6cbdf8846958f80a897f92a8d02ae3
an <test@gmail.com>
2019-12-25 15:53:14 +0700

    first commit

---
+++ Setup/InstallData.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AN\Popup\Setup;
+
+use Magento\Cms\Model\BlockFactory;
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+
+class InstallData implements InstallDataInterface
+{
+    private $blockFactory;
+
+    public function __construct(BlockFactory $blockFactory)
+    {
+        $this->blockFactory = $blockFactory;
+    }
+
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $cmsBlockData = [
+            'title' => 'Custom CMS Block',
+            'identifier' => 'koian',
+            'content' => "<h3>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</h3>",
+            'is_active' => 1,
+            'stores' => [0],
+            'sort_order' => 0
+        ];
+
+        $this->blockFactory->create()->setData($cmsBlockData)->save();
+    }
+} 
